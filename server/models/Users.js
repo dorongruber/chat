1b5fcf66fac28277a0a231db93ede1336e889886
doron <doron.gruber@gmail.com>
2022-03-25 18:50:29 +0300

    update

--- server/models/Users.js
+++ server/models/Users.js
@@ -38,44 +38,4 @@ const UserSchema = mongoose.Schema({
   }
 });
 
-// UserSchema.methods.GetUser = async function(id) {
-//   try {
-//     const user = await this.findOne({ id });
-//     if (!user) return;
-//     return user;
-//   } catch (err) {
-//     throw err;
-//   }
-// }
-
-// UserSchema.methods.SaveUser = async function(id,name,password) {
-//   const user = await this.find({ id });
-//   if (user)
-//     return;
-//   const nweUser = new UserSchema({ id: id, name: name, password: password });
-//   return nweUser.save((err) => {
-//     throw new Error('saving failed');
-//   });
-// }
-// // set first arg as id
-// UserSchema.methods.UpdateUser = function(args) {
-//   this.findOneAndUpdate({id: args[0]}, {...args})
-//   .then(state => {
-//     console.log('is user updated -> ', state);
-//   })
-//   .catch(err => {
-//     throw err;
-//   })
-// }
-
-// UserSchema.methods.DeleteUser = function(id) {
-//   return this.delete({id})
-//   .then(user => {
-//     if (!user) return false;
-//     return true;
-//   }).catch(err => {
-//     throw err;
-//   });
-// }
-
 module.exports = mongoose.model('Users', UserSchema);
--- server/models/messages.js
+++ server/models/messages.js
@@ -35,26 +35,4 @@ const MessageSchema = mongoose.Schema({
   }
 });
 
-// MessageSchema.methods.get = async (id) => {
-//   try {
-//     const message = await this.find({ id });
-//     if (!message)
-//       return;
-//     return message;
-//   } catch (err) {
-//     throw err;
-//   }
-// }
-
-// MessageSchema.methods.delete = (id) => {
-//   return this.delete({id: id})
-//   .then(message => {
-//     if (!message) return false;
-//     return true;
-//   })
-//   .catch(err => {
-//     throw err;
-//   });
-// }
-
 module.exports = mongoose.model('Messages', MessageSchema);
