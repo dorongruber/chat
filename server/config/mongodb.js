3cfab491a725b0a79a5cdfaf895837dc29412023
doron <doron.gruber@gmail.com>
2022-03-26 09:49:03 +0300

    update

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-module.exports = mongoose.connect(process.env.DEV_DB_CONNECTION,
+module.exports = mongoose.connect(process.env.DEV_DB_CONNECTION || PRO_BD_CONNECTION,
 {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
   console.log('connected to db');
 }).catch((err) => {
