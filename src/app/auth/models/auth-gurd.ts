fbcef179c48ccc086af02c5163986009a3c76292
doron <doron.gruber@gmail.com>
2023-09-04 12:06:11 +0300

    update angular version to 16

--- src/app/auth/models/auth-gurd.ts
+++ src/app/auth/models/auth-gurd.ts
@@ -1,11 +1,11 @@
 import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
 import { Observable } from "rxjs";
 import { map, take } from "rxjs/operators";
 import { AuthService } from "src/app/services/auth.service";
 
 @Injectable({ providedIn: 'root'})
-export class AuthGurd implements CanActivate {
+export class AuthGurd  {
 
   constructor(
     private authService: AuthService,
