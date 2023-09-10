6e15251fd02ca2f9a7fdafa4c6add5ad3af2355a
doron <doron.gruber@gmail.com>
2023-09-10 19:00:17 +0300

    shared.module.ts => update material imports from legacy and delete DynamicFormComponent

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 
 import { AlertComponent } from './components/alert/alert.component';
 import { LoadingSpinnerComponent } from './components/loading-spinner/loading-spinner.component';
-import { DynamicFormComponent } from './dynamic-form/dynamic-form.component';
+
 
 import { MatIconModule } from '@angular/material/icon';
-import { MatLegacyFormFieldModule as MatFormFieldModule } from '@angular/material/legacy-form-field';
-import { MatLegacyMenuModule as MatMenuModule } from '@angular/material/legacy-menu';
-import { MatLegacyAutocompleteModule as MatAutocompleteModule } from '@angular/material/legacy-autocomplete';
-import { MatLegacyInputModule as MatInputModule } from '@angular/material/legacy-input';
-import {MatLegacyButtonModule as MatButtonModule} from '@angular/material/legacy-button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule} from '@angular/material/button';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FocusDirective } from './directives/focuslastelement.directive';
@@ -19,7 +19,6 @@ import { Page404Component } from './components/page404/page404.component';
   declarations: [
     AlertComponent,
     LoadingSpinnerComponent,
-    DynamicFormComponent,
     FocusDirective,
     Page404Component,
   ],
@@ -37,7 +36,6 @@ import { Page404Component } from './components/page404/page404.component';
   exports: [
     AlertComponent,
     LoadingSpinnerComponent,
-    DynamicFormComponent,
     MatIconModule,
     MatFormFieldModule,
     MatInputModule,
