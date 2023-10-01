b95bd1373a8f0006037c91d4af239b89c6402f9c
dorongruber <doron.gruber@gmail.com>
2023-10-01 16:46:18 +0300

    main component start refactor

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -4,12 +4,9 @@ import { SocketService } from '../services/socket.service';
 import { UserService } from '../services/user.service';
 import { User } from 'src/app/shared/models/user';
 import { DeviceTypeService } from '../services/devicetype.service';
-import { ControllerService } from '../services/base/controller.service';
-import { NavigationEnd, Router } from '@angular/router';
-import { SubscriptionContolService } from '../services/subscription-control.service';
-import { takeUntil, take, map } from "rxjs/operators";
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 
-const COMPONENT_BASE_ROUTE = '/main';
+
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
@@ -18,27 +15,26 @@ const COMPONENT_BASE_ROUTE = '/main';
 })
 export class MainComponent implements OnInit {
   title = "Landing page";
-  baseRoute = COMPONENT_BASE_ROUTE;
+  
   user: User | undefined;
   isMenuOpen = false;
   menuOption = 3;
   constructor(
     private router: Router,
+    private route: ActivatedRoute,
     private userService: UserService,
     private socketService: SocketService,
     private chatService: ChatsService,
     private deviceTypeService: DeviceTypeService,
-    private controllerService: ControllerService,
-    private subscriptionContolService: SubscriptionContolService,
   ) {
-    this.controllerService.onMenuStateChange
-      .pipe(takeUntil(this.subscriptionContolService.stop$))
-      .subscribe(obj => {this.isMenuOpen = obj.state;});
       this.router.events.subscribe(res => {
         if(res instanceof NavigationEnd) {
           this.title = res.url.split('/')[2];
         }
-      });
+      });      
+      if(this.deviceTypeService.isMobile) {
+        this.router.navigate(["menu"], {relativeTo: this.route.parent});
+      } 
    }
 
   async ngOnInit(): Promise<void> {
