30877324f8b5c6f1bf6105f6ff7ff156e9b70b5f
dorongruber <doron.gruber@gmail.com>
2023-10-03 20:23:45 +0300

    add route prop to menu option lists

--- src/app/mockData/menuoptionslists.ts
+++ src/app/mockData/menuoptionslists.ts
@@ -1,11 +1,10 @@
 export const chatMenuOptions = [
-  {title: 'chat info', icon: 'info'},
-  {title: 'exit chat', icon: 'chevron_right'},
+  {title: 'chat info', icon: 'info', route: '/main/chatinfo'},
+  {title: 'exit chat', icon: 'chevron_right', route: 'auth'},
 ];
 export const mainMenuOptions = [
-  {title: 'create chat', icon: 'add_circle_outline'},
-  {title: 'remove chat', icon: 'remove_circle_outline'},
-  {title: 'user info', icon: 'info'},
-  {title: 'chats', icon: 'chat'},
-  {title: 'exit app', icon: 'exit_to_app'},
+  {title: 'create chat', icon: 'add_circle_outline', route: '/main/new'},
+  {title: 'remove chat', icon: 'remove_circle_outline', route: '/main/delete'},
+  {title: 'user info', icon: 'info', route: '/main/userinfo'},
+  {title: 'exit app', icon: 'exit_to_app', route: 'auth'},
 ];
