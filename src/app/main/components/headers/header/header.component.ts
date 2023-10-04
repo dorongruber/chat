4ca6e7cb947e90d149f1de65e59a574ed8c4d47c
dorongruber <doron.gruber@gmail.com>
2023-10-04 12:11:28 +0300

    move header to headers folder

---
+++ src/app/main/components/headers/header/header.component.ts
@@ -0,0 +1,31 @@
+import { Component, Input, OnChanges, ViewChild } from '@angular/core';
+import { ComponentDirective } from '../../../directives/component.directive';
+import { DynamicComponentRef } from '../../../directives/dynamic-component.ref.directive';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnChanges {
+  isChatOpen = false;
+  @Input() menuOptions: any[] = [];
+  @Input() title: string = '';
+  
+  @Input() componentRef?: DynamicComponentRef | undefined;
+  @ViewChild(ComponentDirective, { static: true}) bodyHost?: ComponentDirective;
+
+  constructor() { }
+
+  ngOnChanges(): void {
+    if(this.componentRef && this.bodyHost) {
+      this.bodyHost.viewContainerRef.clear();
+      const ref = this.bodyHost.viewContainerRef.createComponent(this.componentRef.bodyComponent);
+      console.log("ref ==> ", ref.instance);
+      ref.instance.menuOptions = this.menuOptions;
+      ref.instance.title = this.title;
+    }
+  }
+  
+
+}
