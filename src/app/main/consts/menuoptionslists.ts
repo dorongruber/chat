dc7b68cf8ee8e7a2eee6393bead933d35804824c
dorongruber <doron.gruber@gmail.com>
2023-10-06 13:07:38 +0300

    change menuoptionlist.ts

--- src/app/main/consts/menuoptionslists.ts
+++ src/app/main/consts/menuoptionslists.ts
@@ -1,16 +1,19 @@
 import { DeletechatComponent } from "../components/deletechat/deletechat.component";
+import { GroupchatComponent } from "../components/groupchat/groupchat.component";
 import { UserinfoComponent } from "../components/userinfo/userinfo.component";
 import { DynamicComponentRef } from "../directives/dynamic-component.ref.directive";
 import { HeaderMenuOption } from "../models/header-menu-option";
 import { NewchatComponent } from "../routes/newchat/newchat.component";
 
 export const chatMenuOptions = [
-  new HeaderMenuOption('chat info', 'info', '/main/chatinfo', new DynamicComponentRef(NewchatComponent)),
-  new HeaderMenuOption('exit chat', 'chevron_right','auth'),
+  new HeaderMenuOption('chat info', 'info', new DynamicComponentRef(NewchatComponent)),
+  new HeaderMenuOption('exit chat', 'chevron_right'),
 ];
 export const mainMenuOptions = [
-  new HeaderMenuOption('create chat', 'add_circle_outline', '/main/new',  new DynamicComponentRef(NewchatComponent)),
-  new HeaderMenuOption('remove chat', 'remove_circle_outline', '/main/delete', new DynamicComponentRef(DeletechatComponent)),
-  new HeaderMenuOption('user info', 'info', '/main/userinfo', new DynamicComponentRef(UserinfoComponent)),
-  new HeaderMenuOption('exit app', 'exit_to_app','auth'),
+  new HeaderMenuOption('new group', 'add_circle_outline',  new DynamicComponentRef(GroupchatComponent)),
+  new HeaderMenuOption('remove chat', 'remove_circle_outline', new DynamicComponentRef(DeletechatComponent)),
+  new HeaderMenuOption('exit app', 'exit_to_app'),
 ];
+
+export const UserIcon = new HeaderMenuOption('user info', 'info', new DynamicComponentRef(UserinfoComponent));
+export const newChatIcon = new HeaderMenuOption('new chat', 'add_circle_outline',  new DynamicComponentRef(NewchatComponent));
