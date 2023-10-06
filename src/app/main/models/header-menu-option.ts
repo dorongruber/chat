7f1b01af360d3e2caef4c07353f0b69fbe864ce3
dorongruber <doron.gruber@gmail.com>
2023-10-06 13:04:43 +0300

    HeaderMenuOption

--- src/app/main/models/header-menu-option.ts
+++ src/app/main/models/header-menu-option.ts
@@ -3,12 +3,10 @@ import { DynamicComponentRef } from "../directives/dynamic-component.ref.directi
 export class HeaderMenuOption {
     title: string;
     icon: string;
-    path: string;
     componentRef?: DynamicComponentRef;
-    constructor(title: string, icon: string, path: string, componentRef: DynamicComponentRef | undefined = undefined) {
+    constructor(title: string, icon: string, componentRef: DynamicComponentRef | undefined = undefined) {
         this.title = title;
         this.icon = icon;
-        this.path = path;
         this.componentRef = componentRef;
     }
 }
